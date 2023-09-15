7ea7c9cfa151c38058eca690633d06d77b9df195
Bhushan Shah <bhushan.shah.dev@gmail.com>
2023-09-14 21:40:38 -0700

    Fixed API route to use new type

--- src/app/api/upload-rules-file/route.ts
+++ src/app/api/upload-rules-file/route.ts
@@ -8,16 +8,18 @@ export async function POST(request: NextRequest) {
   // FIXME: do something with the file to calculate the response
   // response mocked for now
 
-  const response: RulesEncoding = {
-    "Section 1": {
+  const response: RulesEncoding = [
+    {
+      sectionName: "Section 1",
       plaintext: "Section 1 text",
       code: 'print "Section 1"',
     },
-    "Section 2": {
+    {
+      sectionName: "Section 2",
       plaintext: "Section 2 text",
       code: 'print "Section 2"',
     },
-  };
+  ];
 
   return NextResponse.json(response);
 }
